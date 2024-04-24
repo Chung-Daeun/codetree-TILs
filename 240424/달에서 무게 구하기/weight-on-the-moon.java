4335d7ae3c7db63691ca481629a23bd98f506d6c
Chung-Daeun <108713683+Chung-Daeun@users.noreply.github.com>
2024-04-24 14:20:29 +0900

    Title: 달에서 무게 구하기, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240424/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int weight = 13;
+        float rate = 0.165f;
+
+        System.out.printf("%d * %.6f = %.6f", weight, rate, (weight*rate)); 
+    }
+}
