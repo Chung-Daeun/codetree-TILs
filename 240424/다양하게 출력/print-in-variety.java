078656c4c16d72a738ce7c262795be27e454ea65
Chung-Daeun <108713683+Chung-Daeun@users.noreply.github.com>
2024-04-24 14:02:50 +0900

    Title: 다양하게 출력, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240424/다양하게 출력/print-in-variety.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        System.out.print("Total days in Year\n");
+        System.out.println(365);
+        System.out.print("Circumference rate\n");
+        System.out.println(3.1415926535);
+    }
+}
