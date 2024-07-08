b43c9073da572d6538af68cd848177bdd36ad3b0
Chung-Daeun <108713683+Chung-Daeun@users.noreply.github.com>
2024-07-08 14:02:10 +0900

    Title: 세 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240708/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int min = (a < b)?
+                (a < c)? a: c
+                    :
+                (b < c)? b : c;
+
+    cout << min;
+    return 0;
+}
