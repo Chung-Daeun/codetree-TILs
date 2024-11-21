f5a0847ea8bec25ee2c112cc7983f4ed78be3ab1
ChungDaeun <108713683+Chung-Daeun@users.noreply.github.com>
2024-11-21 17:47:09 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241121/두 사람/two-person.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int age[2];
+    char gender[2];
+
+    for(int i = 0; i < 2; i++){
+        cin >> age[i];
+        cin >> gender[i];
+    }
+
+    if((age[0] >= 19 && gender[0] == 'M') || (age[1] >= 19 && gender[0] == 'M')){
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    return 0;
+}
