ebf79db3f1cfd63927d8a6e399f1c30656085bdb
ChungDaeun <108713683+Chung-Daeun@users.noreply.github.com>
2025-01-11 04:32:46 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250111/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int a, b;
+
+bool prime(int n) {
+    for (int i = 2; i <= static_cast<int>(sqrt(n)); i++){
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+int main() {
+    cin >> a >> b;
+
+    // Write your code here!
+    int sum = 0;
+    for (int i = a; i <= b; i++) {
+        if (prime(i)) {
+            sum += i;
+        }
+    }
+
+    cout << sum;
+    return 0;
+}
