f41353d4d7be4e597e1b2f1238f6c46a57e27ddb
ChungDaeun <108713683+Chung-Daeun@users.noreply.github.com>
2025-01-11 04:35:31 +0900

    Title: 함수를 이용한 소수 판별, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250111/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 250111/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -7,7 +7,7 @@ int a, b;
 bool prime(int n) {
 
     if (n == 1) return false;
-    for (int i = 2; i <= sqrt(n) + 1; i++){
+    for (int i = 2; i <= n; i++){
         if (n % i == 0) {
             return false;
         }
