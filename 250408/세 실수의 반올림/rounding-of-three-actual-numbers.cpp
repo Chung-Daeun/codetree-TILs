f8a1b30567f5d391651cbab4800fddd475c76949
ChungDaeun <108713683+Chung-Daeun@users.noreply.github.com>
2025-04-08 22:06:09 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250408/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a, b, c;
+
+    cout << fixed;
+    cout.precision(3);
+
+    cin >> a >> b >> c;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    return 0;
+}
