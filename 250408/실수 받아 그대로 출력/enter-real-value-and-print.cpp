8d55ebe09a757157d966f33e4350da8695495723
ChungDaeun <108713683+Chung-Daeun@users.noreply.github.com>
2025-04-08 22:02:53 +0900

    Title: 실수 받아 그대로 출력, Time: 2ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250408/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    cout.precision(2);
+
+    double N;
+    cin >> N;
+    cout << N;
+    return 0;
+}
