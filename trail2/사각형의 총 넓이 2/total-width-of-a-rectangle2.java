cfd016dba476b889895008050f77cd662d68b84a
ChungDaeun <108713683+Chung-Daeun@users.noreply.github.com>
2026-06-02 18:08:07 +0900

    [Runtime Error] 사각형의 총 넓이 2 (96ms, 9MB)

---
+++ trail2/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[][] grid = new int[205][205];
+    static int area = 0;
+
+    static void input() throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+        checkArea(x1, y1, x2, y2);
+    }
+
+    static void checkArea(int x1, int y1, int x2, int y2) {
+        for (int r = x1; r < x2; r++) {
+            for (int c = y1; c < y2; c++) {
+                grid[r][c] = 1;
+            }
+        }
+    }
+
+    static void calcArea() {
+        for (int i = 0; i < 205; i++) {
+            for (int j = 0; j < 205; j++) {
+                if (grid[i][j] == 1)    area++;
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            input();
+        }
+        br.close();
+
+        calcArea();
+        System.out.print(area);
+    }
+}
